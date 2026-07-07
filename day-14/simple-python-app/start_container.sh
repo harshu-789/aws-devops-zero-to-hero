464e4bc7c3c349109f6a9b0477e10ce4f6596e72
Rishesha Harsh <137793831+harshu-789@users.noreply.github.com>
2026-07-07 22:48:13 +0530

    Fix syntax errors in start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull harshu789/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 harshu789/simple-python-flask-app
                          
