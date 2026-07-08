661049bcf840acc7b84cb28dd566e3b3d41edcfa
Rishesha Harsh <137793831+harshu-789@users.noreply.github.com>
2026-07-08 17:05:16 +0530

    Update Docker commands to use 'latest' tag

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker pull harshu789/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 8000:8000 harshu789/simple-python-flask-app
-                         
+docker pull harshu789/simple-python-flask-app:latest
+docker run -d -p 8000:8000 harshu789/simple-python-flask-app:latest
