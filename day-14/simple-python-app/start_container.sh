54eacb24f0bb800af7b3c300be834c20e55a2457
Rishesha Harsh <137793831+harshu-789@users.noreply.github.com>
2026-07-08 17:06:02 +0530

    Change port mapping from 8000 to 5000

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker pull harshu789/simple-python-flask-app:latest
-docker run -d -p 8000:8000 harshu789/simple-python-flask-app:latest
+docker run -d -p 5000:5000 harshu789/simple-python-flask-app:latest
