26d2651a2f159050c578390c7d6c34da0efee87f
Rishesha Harsh <137793831+harshu-789@users.noreply.github.com>
2026-07-08 17:40:40 +0530

    Improve docker run command formatting
    Refactor docker run command for better readability.

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,4 +2,8 @@
 set -e
 
 docker pull harshu789/simple-python-flask-app:latest
-docker run -d -p 5000:5000 harshu789/simple-python-flask-app:latest
+
+docker run -d \
+  --name simple-python-flask-app \
+  -p 5000:5000 \
+  harshu789/simple-python-flask-app:latest
